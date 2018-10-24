45e6e4d36bd80038b5cfd87d1d51e5963499127b
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-10-24 14:41:35 +0200

    x
    x

--- AbaxOppgave/Transportmiddel.cs
+++ AbaxOppgave/Transportmiddel.cs
@@ -1,15 +1,26 @@
-﻿namespace AbaxOppgave
+﻿using System.Collections.Generic;
+
+namespace AbaxOppgave
 {
     public class Transportmiddel
     {
-        protected Transportmiddel(string kjennetegn, decimal maksimalfart, decimal effekt, Transportmiddeltype transportmiddeltype)
-        {
-            
-        }
-
         public string Kjennetegn { get; }
         public decimal? Maksimalfart { get; }
         public decimal Effekt { get; }
         public Transportmiddeltype? Type { get; }
+
+        protected Dictionary<string, string> Enheter = new Dictionary<string, string>
+        {
+            {nameof(Maksimalfart), "kmt/t"},
+            {nameof(Effekt), "kw"},
+        };
+
+        protected Transportmiddel(string kjennetegn, decimal maksimalfart, decimal effekt, Transportmiddeltype? type)
+        {
+            Kjennetegn = kjennetegn;
+            Maksimalfart = maksimalfart;
+            Effekt = effekt;
+            Type = type;
+        }
     }
 }
