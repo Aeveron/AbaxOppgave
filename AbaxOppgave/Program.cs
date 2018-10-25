de9351c5bd3fb4f293f8a5cb9e287fb409010bdf
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-10-25 10:36:44 +0200

    x
    x

--- AbaxOppgave/Program.cs
+++ AbaxOppgave/Program.cs
@@ -8,10 +8,18 @@ namespace AbaxOppgave
         {           
             var bil1 = new Bil("NF123456", 147, 200, "grønn", Transportmiddeltype.LettKjøretøy);
             bil1.Print();
+            bil1.Kjør();
+            Console.WriteLine();
             var bil2 = new Bil("NF654321", 150, 195, "blå", Transportmiddeltype.LettKjøretøy);
             bil2.Print();
 
-            
+            var fly1 = new Fly("LN1234", 1000, 30, 2000, 10000, Transportmiddeltype.Jetfly);
+            fly1.Print();
+            fly1.StartFly();
+
+            var båt1 = new Båt("ABC123", 30, 100, 500);
+            båt1.Print();
+          
         }
     }
 }
